f53abf08874f50d0de57b6a76e48670432b69728
Jesse Bryan <jesse.bryan22@gmail.com>
2026-05-13 16:26:45 -0700

    Add: reorder-linked-list - submission-3

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-3.js
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {void}
+     */
+    reorderList(head) {
+        let midPoint = this.findMidPointLinkedList(head);
+        let reversedSecondHalfHead = this.reverseLinkedList(midPoint);
+        while (head && reversedSecondHalfHead) {
+            let temp = head.next;
+            let temp2 = reversedSecondHalfHead.next;
+            head.next = reversedSecondHalfHead;
+            head = temp;
+            reversedSecondHalfHead.next = head;
+            reversedSecondHalfHead = temp2;
+        }
+    }
+
+    findMidPointLinkedList(head) {
+        let pointerA = head;
+        let pointerB = head.next;
+
+        while (pointerB && pointerB.next) {
+            pointerB = pointerB.next.next;
+            pointerA = pointerA.next;
+        }
+        return pointerA;
+    }
+
+    reverseLinkedList(head) {
+        let prev = null;
+        let curr = head;
+
+        while (curr) {
+            let temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        return prev;
+    }
+}
