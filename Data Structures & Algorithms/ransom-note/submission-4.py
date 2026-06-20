c272b2b5f11465d039461ce15775615af76a4d15
Jesse Bryan <jesse.bryan22@gmail.com>
2026-06-19 18:47:38 -0700

    Add: ransom-note - submission-4

---
+++ Data Structures & Algorithms/ransom-note/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        mag_chars = {}
+
+        for c in magazine:
+            mag_chars[c] = mag_chars.get(c, 0) + 1
+            
+
+        for c in ransomNote:
+            mag_chars[c] = mag_chars.get(c, 0) - 1
+            if mag_chars[c] < 0:
+                return False
+        return True
