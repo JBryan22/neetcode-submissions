715ca486d36df33f9df41e8ab5cac32199060f2d
Jesse Bryan <jesse.bryan22@gmail.com>
2026-07-14 12:12:57 -0700

    Add: path-with-maximum-probability - submission-3

---
+++ Data Structures & Algorithms/path-with-maximum-probability/submission-3.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
+        adjList = defaultdict(list)
+
+        for i in range(len(edges)):
+            s, e = edges[i]
+            adjList[s].append((e, succProb[i]))
+            adjList[e].append((s, succProb[i]))
+
+        maxH = []
+        maxProbs = [0] * n
+        for node, prob in adjList[start_node]:
+            heapq.heappush(maxH, (-1 * prob, node))
+            maxProbs[node] = 1 * prob
+        
+        while maxH:
+            neg_p, node = heapq.heappop(maxH)
+            p = -neg_p
+
+            if node == end_node:
+                return p
+            
+            for n, prob in adjList[node]:
+                if maxProbs[n] < p * prob:
+                    heapq.heappush(maxH, (-(p * prob), n))
+                    maxProbs[n] = p * prob
+
+        return 0
