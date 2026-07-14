2ca06041742e1fc90717fe098c8c6aa7b54bb087
Jesse Bryan <jesse.bryan22@gmail.com>
2026-07-14 11:15:02 -0700

    Add: swim-in-rising-water - submission-1

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        minHeap = [(0,(0,0))]
+        ROW_LENGTH = len(grid)
+        COL_LENGTH = len(grid[0])
+
+        time = 0
+        directions = [(0,1), (0,-1), (1, 0), (-1, 0)]
+
+        while minHeap:
+            _, rc = heapq.heappop(minHeap)
+            (r, c) = rc
+            if grid[r][c] > time:
+                time = grid[r][c]
+            
+            if r == ROW_LENGTH - 1 and c == COL_LENGTH - 1:
+                return time
+            for dr, dc in directions:
+                if not (r + dr < 0 or r + dr >= ROW_LENGTH or c + dc < 0 or c + dc >= COL_LENGTH or grid[r + dr][c + dc] == -1):
+                    heapq.heappush(minHeap, (grid[r+dr][c+dc],(r + dr, c + dc)))
+                    grid[r+dr][c+dc] = -1 
+        
+        return time
+
