aaf413d903370a89a8a5a5356bf32ac5f0bf84c4
Jesse Bryan <jesse.bryan22@gmail.com>
2026-07-20 00:01:46 -0700

    Add: combination-target-sum - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        subsets = []
+
+        def sumPaths(i: int, currSum: int, currSubset: List[int]):
+            if currSum == target:
+                subsets.append(currSubset.copy())
+                return
+
+            for j in range(i, len(nums)):
+                if currSum + nums[j] > target:
+                    return
+                
+                currSubset.append(nums[j])
+                sumPaths(j, currSum + nums[j], currSubset)
+                currSubset.pop()
+
+        sumPaths(0, 0, [])
+        return subsets
