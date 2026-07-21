7a1687304e1a92af8bad82de24e748daf8ad3ede
Jesse Bryan <jesse.bryan22@gmail.com>
2026-07-21 15:40:28 -0700

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        sets = []
+
+        def genParen(currStr: str, left_count: int, right_count: int):
+            if left_count + right_count == (n * 2) and left_count == right_count:
+                sets.append(currStr)
+                return
+            if right_count > left_count or left_count + right_count >= (n * 2):
+                return
+
+            currStr += "("
+            genParen(currStr, left_count + 1, right_count)
+            currStr = currStr[:-1]
+
+            currStr += ")"
+            genParen(currStr, left_count, right_count + 1)
+            currStr = currStr[:-1]
+        
+        genParen("", 0, 0)
+        return sets
