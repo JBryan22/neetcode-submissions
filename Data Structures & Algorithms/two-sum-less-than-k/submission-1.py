72c93ccd34d925504374c6c8ad1250c14dd7fa97
Jesse Bryan <jesse.bryan22@gmail.com>
2026-08-27 11:07:32 -0700

    Add: two-sum-less-than-k - submission-1

---
+++ Data Structures & Algorithms/two-sum-less-than-k/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def twoSumLessThanK(self, nums: List[int], k: int) -> int:
+        if len(nums) < 2:
+            return nums[0]
+
+        numsSorted = sorted(nums)
+
+        l, r = 0, len(nums) - 1
+        maxNum = -1
+
+        while l < r:
+            s = numsSorted[l] + numsSorted[r]
+
+            if s >= k:
+                r -= 1
+            else:
+                maxNum = max(maxNum, s)
+                l += 1
+        return maxNum
+
