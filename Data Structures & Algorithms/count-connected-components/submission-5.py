bfb0e233c654f9b42e7380c34bdd480702fe60e7
Jesse Bryan <jesse.bryan22@gmail.com>
2026-09-04 21:23:51 -0700

    Add: count-connected-components - submission-5

---
+++ Data Structures & Algorithms/count-connected-components/submission-5.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        count = 0
+        adjList = defaultdict(list)
+
+        for node1, node2 in edges: 
+            adjList[node1].append(node2)
+            adjList[node2].append(node1)
+        
+        visited = set()
+
+        for node in adjList:
+            if node in visited:
+                continue
+            
+            stack = [node]
+            visited.add(node)
+
+            while stack:
+                innerNode = stack.pop()
+                for nei in adjList[innerNode]:
+                    if nei not in visited:
+                        stack.append(nei)
+                        visited.add(nei)
+            count += 1
+        return count
+            
